b43eebb59e7b9b4f2721a8775e1dcded7922e270
rozina19405 <159819449+rozina19405@users.noreply.github.com>
2024-02-29 08:26:52 +0300

    Add files via upload

---
+++ INSPIRE STEM/WEEK_3/tuple_hobbies.py
@@ -0,0 +1,7 @@
+# this is a program to list my hobbies
+# date : 28/02/2024
+# name : Rozina Adhiambo
+
+hobbies = ("Drawing", "skating","gaming","watching movies","crotcheting")
+
+print(hobbies)
---
+++ INSPIRE STEM/WEEK_3/tuples.py
@@ -0,0 +1,14 @@
+# tuples
+
+cities = ("Kisumu", "Nairobi","Nakuru","Kampala","Mombasa","Dar es Salaam")
+
+print(cities)
+
+#cities.append("Juba")
+ # returns that tuple object has no attribute append
+
+#cities.sort
+ # returns that tuple object has no attribute sort
+
+for city in cities:
+    print(city)
